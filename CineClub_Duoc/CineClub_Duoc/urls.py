e29c9a61a5452e1def0047dacf131885cbc3936e
jaacortesp <ja.cortesp@duocuc.cl>
2024-04-23 01:25:27 -0400

    Update

--- CineClub_Duoc/CineClub_Duoc/urls.py
+++ CineClub_Duoc/CineClub_Duoc/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for TestDjango project.
+URL configuration for CineClub_Duoc project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
--- CineClub_Duoc/CineClub_Duoc/wsgi.py
+++ CineClub_Duoc/CineClub_Duoc/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for TestDjango project.
+WSGI config for CineClub_Duoc project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TestDjango.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'CineClub_Duoc.settings')
 
 application = get_wsgi_application()
